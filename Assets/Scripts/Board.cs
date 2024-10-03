228ba404b98fae9224f0a7cb0e4da410ff7000ab
NathanW97 <ngw@live.com.au>
2024-10-03 18:26:43 +1000

    Adding Game to GitHub
    Moving the game into the github folder

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderKeywordFilter;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Board : MonoBehaviour
+{
+    public Tilemap tilemap { get; private set; }
+    public Piece activePiece { get; private set; }
+    public TetrominoData[] tetrominos;
+    public Vector3Int spawnPosition;
+    public Vector2Int boardSize = new Vector2Int(10, 20);
+
+    public RectInt Bounds
+    {
+        get
+        {
+            Vector2Int position = new Vector2Int(-5, -10);
+            return new RectInt(position, boardSize);
+        }
+    }
+
+    private void Awake()
+    {
+        tilemap = GetComponentInChildren<Tilemap>();
+        activePiece = GetComponentInChildren<Piece>();
+        for (int i = 0; i < tetrominos.Length; i++) {  
+            tetrominos[i].Initialize();
+        }
+    }
+
+    private void Start()
+    {
+        SpawnPiece();
+    }
+
+    public void SpawnPiece()
+    { 
+        int random = Random.Range(0,tetrominos.Length);
+        TetrominoData data = tetrominos[random];
+
+        activePiece.Initialize(this, spawnPosition, data);
+        Set(activePiece);
+    }
+
+    public void Set(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            tilemap.SetTile(tilePosition, piece.data.tile);
+        }
+    }
+
+    public void Clear(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            tilemap.SetTile(tilePosition, null);
+        }
+    }
+
+    public bool IsValidPosition(Piece piece, Vector3Int position)
+    {
+        RectInt bounds = Bounds;    
+
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+
+            // check if piece location is occupied 
+            if (tilemap.HasTile(tilePosition)) 
+            {
+                return false;
+            }
+
+            // check if out-of-bounds
+            if (!bounds.Contains((Vector2Int)tilePosition))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
